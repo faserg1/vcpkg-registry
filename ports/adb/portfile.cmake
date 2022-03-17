6c020322cda4e201c66e67acea55a380b435a61e
HrOokami <dev@hrookami.icu>
2022-03-17 13:04:17 +0300

    Editing package

--- ports/adb/portfile.cmake
+++ ports/adb/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO faserg1/adb
-  REF 490da40df86526f6a7a8ea6437397c05ac3bbb71
-  SHA512 0
+  REF 8b0bd7539bd1d121478ea6353eb369d0efb2e3ea
+  SHA512 787640fcdbfd7ba9180c7cb18193677b49c348e0ac8d6c76f2a8d56dc512fe972b65185135037fd124aee5811333b760a92ef84246162ffd870c75e9667d50be
   HEAD_REF master
 )
 
@@ -11,11 +11,6 @@ vcpkg_configure_cmake(
   PREFER_NINJA
 )
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_fixup_cmake_targets(CONFIG_PATH "share/libadb")
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-
-file(
-  INSTALL "${SOURCE_PATH}/LICENSE"
-  DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
-  RENAME copyright)
